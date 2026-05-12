17c6ef803d33761210c6de7513d301fc69ed197a
jagruti <jagrutibiswal2025@gmail.com>
2026-05-12 15:08:13 +0530

    debugging databse init url

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pkg;
+console.log("Attempting to connect with URL:", process.env.DATABASE_URL?.replace(/:([^:@]+)@/, ':****@'));
 
 const db = new Pool({
   connectionString: process.env.DATABASE_URL
