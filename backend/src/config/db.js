726f30432b4556b10abbd38f35a8a8a2c7e7a176
jagruti <jagrutibiswal2025@gmail.com>
2026-05-12 15:30:29 +0530

    debugging databse init url-1.0

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -7,7 +7,10 @@ const { Pool } = pkg;
 console.log("Attempting to connect with URL:", process.env.DATABASE_URL?.replace(/:([^:@]+)@/, ':****@'));
 
 const db = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 export default db;
